67f7962b1fa7ba9410cd37a9c6adbfc4ec4ae8a1
kk5741697-ai <kk5741697@gmail.com>
2025-09-10 00:05:03 +0600

    Delete components directory

--- components/ads/index.ts
+++
@@ -1,4 +0,0 @@
-// Central exports for ad components
-export { AdBanner } from './ad-banner'
-export { PersistentAdBanner, persistentAdManager } from './persistent-ad-manager'
-export { MobileAdManager } from './mobile-ad-manager'
